77b1acb9dd6543de46899f224e8a2f307506d280
Paul Yu <paul.d.yu@gmail.com>
2023-06-02 16:30:11 -0700

    feat: adding virtual-worker to process orders

---
+++ src/virtual-worker/app.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const path = require('path')
+const AutoLoad = require('@fastify/autoload')
+
+const {
+  ORDER_QUEUE_PROTOCOL = 'amqp',
+  ORDER_QUEUE_HOSTNAME = 'localhost',
+  ORDER_QUEUE_PORT = 5672,
+  ORDER_QUEUE_USERNAME = 'username',
+  ORDER_QUEUE_PASSWORD = 'password',
+  ORDER_DB_URL = 'mongodb://127.0.0.1:27017/orderdb'
+} = process.env
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+  fastify.register(require('fastify-amqp'), {
+    protocol: ORDER_QUEUE_PROTOCOL,
+    hostname: ORDER_QUEUE_HOSTNAME,
+    port: ORDER_QUEUE_PORT,
+    username: ORDER_QUEUE_USERNAME,
+    password: ORDER_QUEUE_PASSWORD
+  })
+
+  fastify.register(require('@fastify/mongodb'), {
+    forceClose: true,
+    url: ORDER_DB_URL
+  })
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+}
