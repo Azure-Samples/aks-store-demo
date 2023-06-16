7e5dcde5cebd9bc7d6ac547236985ee40a508967
Paul Yu <paul.d.yu@gmail.com>
2023-06-16 11:16:31 -0700

    feat: ui clean up in store-admin

--- src/store-admin/src/components/ProductDetail.vue
+++ src/store-admin/src/components/ProductDetail.vue
@@ -1,15 +1,19 @@
 <template>
+  <div class="action-button">
+    <router-link :to="`/product/${this.$route.params.id}/edit`">
+      <button class="button">Edit Product</button>
+    </router-link>
+  </div>
+  <br/>
   <div class="product-detail" v-if="productExists">
-    <div class="action-button">
-      <router-link :to="`/product/${this.$route.params.id}/edit`">
-        <button class="button">Edit Product</button>
-      </router-link>
+    <div class="product-image">
+      <img :src="product.image" alt="Product Image">
     </div>
-    <br/>
-    <p>Product ID: {{ product.id }}</p>
-    <h3>{{ product.name }} - {{ product.price }}</h3>
-    <p><img :src="product.image" alt="Product Image"></p>
-    <p>{{ product.description }}</p>
+    <div class="product-info">
+      <h2>{{ product.name }} - {{ product.price }}</h2>
+      <small>Product ID: {{ product.id }}</small>
+      <p>{{ product.description }}</p>
+    </div>
   </div>
   <div class="product-detail" v-else>
     <h3>Product not found</h3>
@@ -32,9 +36,41 @@
 </script>
 
 <style scoped>
-/* a tag styles */
 a {
   color: #0000FF;
   text-decoration: underline;
+}
+
+.product-detail {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 1rem;
+  margin: 1rem;
+}
+
+.product-image {
+  flex: 1;
+  margin-right: 20px;
+}
+
+.product-image img {
+  width: 100%;
+  height: auto;
+}
+
+.product-info {
+  flex: 1;
+  text-align: left;
+}
+
+.product-info h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.product-info p {
+  font-size: 16px;
+  margin-bottom: 20px;
 }
 </style>
--- src/store-admin/src/components/ProductForm.vue
+++ src/store-admin/src/components/ProductForm.vue
@@ -3,6 +3,12 @@
     <button @click="saveProduct" class="button">Save Product</button>
   </div>
   <br/>
+  <div v-if="showValidationErrors" class="error">
+    <br/>
+    <ul v-for="error in validationErrors" :key="error">
+      <li>{{ error }}</li>
+    </ul>
+  </div>
   <div class="product-form">
     <div class="form-row">
       <label for="product-id">ID</label>
@@ -16,7 +22,7 @@
 
     <div class="form-row">
       <label for="product-price">Price</label>
-      <input id="product-price" placeholder="Product Price" v-model="product.price" />
+      <input id="product-price" placeholder="Product Price" v-model="product.price" type="number" step="0.01" />
     </div>
 
     <div class="form-row">
@@ -51,7 +57,8 @@
           description: '',
           price: 0.00,
           tags: []
-        }
+        },
+        showValidationErrors: false
       }
     },
     mounted() {
@@ -82,6 +89,24 @@
           console.log(error)
           document.getElementsByClassName('ai-button')[0].style.display = 'none';
         })
+    },
+    computed: {
+      validationErrors() {
+        let errors = [];
+        if (this.product.name.length === 0) {
+          errors.push('Please enter a value for the name field');
+        }
+
+        if (this.product.description.length === 0) {
+          errors.push('Please enter a value for the description field');
+        }
+
+        if (this.product.price <= 0) {
+          errors.push('Please enter a value greater than 0 for the price field');
+        }
+
+        return errors;
+      }
     },
     methods: {
       generateDescription() {
@@ -133,6 +158,11 @@
         return intervalId;
       },
       saveProduct() {
+        if (this.validationErrors.length > 0) {
+          this.showValidationErrors = true;
+          return;
+        }
+
         // get the product-service URL from an environment variable
         const productServiceUrl = process.env.VUE_APP_PRODUCT_SERVICE_URL;
 
@@ -176,3 +206,14 @@
     }
   }
 </script>
+
+<style scoped>
+ul {
+  justify-content: center;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  color: #ff0000;
+}
+</style>
