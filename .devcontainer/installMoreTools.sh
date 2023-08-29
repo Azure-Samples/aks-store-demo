a3b18099140b558bde43c88ddbd2dbb01c87c68d
Paul Yu <paul.d.yu@gmail.com>
2023-08-29 00:02:55 +0000

    feat: adding tools and bumping mongosh

--- .devcontainer/installMoreTools.sh
+++ .devcontainer/installMoreTools.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 ###################
+# uuidgen
+###################
+sudo apt-get update && sudo apt-get install uuid-runtime
+
+###################
 # mongodb-mongosh
 ###################
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-mongosh
+wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-7.0.asc
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update && sudo apt-get install -y mongodb-mongosh
