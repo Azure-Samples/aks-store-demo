cbc8b8afbbfcfb6c6c611d81f266255daa10932c
Paul Yu <paul.d.yu@gmail.com>
2024-02-01 15:24:38 -0800

    Update postprovision.sh

--- infra/azd-hooks/postprovision.sh
+++ infra/azd-hooks/postprovision.sh
@@ -2,13 +2,13 @@
 
 services=("ai-service" "makeline-service" "order-service" "product-service" "store-admin" "store-front" "virtual-customer" "virtual-worker")
 
-if [ -n "$DEPLOY_AZURE_CONTAINER_REGISTRY" ] && [ "$BUILD_CONTAINERS" == "true" ]; then
+if [ "$DEPLOY_AZURE_CONTAINER_REGISTRY" == "true" ] && [ "$BUILD_CONTAINERS" == "true" ]; then
   echo "Build container images"
   for service in "${services[@]}"; do
     echo "Building aks-store-demo/${service}:latest"
     az acr build --registry ${registry_name} --image aks-store-demo/${service}:latest ./src/${service}/
   done
-elif [ -n "$DEPLOY_AZURE_CONTAINER_REGISTRY" ] && ([ -z "$BUILD_CONTAINERS" ] || [ "$BUILD_CONTAINERS" == "false" ]); then
+elif [ "$DEPLOY_AZURE_CONTAINER_REGISTRY" == "true" ] && ([ -z "$BUILD_CONTAINERS" ] || [ "$BUILD_CONTAINERS" == "false" ]); then
   echo "Import container images"
   for service in "${services[@]}"; do
     echo "Importing aks-store-demo/${service}:latest"
