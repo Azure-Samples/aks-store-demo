75633fd02fd8b19bea8d238477cc9b793a3618f8
Paul Yu <paul.d.yu@gmail.com>
2024-02-08 14:39:24 -0800

    Update keyvault.tf

--- infra/terraform/keyvault.tf
+++ infra/terraform/keyvault.tf
@@ -1,23 +1,17 @@
 resource "azurerm_key_vault" "example" {
-    name                        = "kv-${local.name}"
+    name                        = "akv-${local.name}"
     location                    = azurerm_resource_group.example.location
     resource_group_name         = azurerm_resource_group.example.name
     enabled_for_disk_encryption = true
     tenant_id                   = data.azurerm_client_config.current.tenant_id
     sku_name                    = "standard"
+    enable_rbac_authorization   = true
 }
 
-resource "azurerm_key_vault_access_policy" "example" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-  secret_permissions = [
-    "Get",
-    "Set",
-    "List",
-    "Delete",
-    "Purge"
-  ]
+resource "azurerm_role_assignment" "example_aoai_me" {
+    principal_id         = data.azurerm_client_config.current.object_id
+    role_definition_name = "Key Vault Administrator"
+    scope                = azurerm_key_vault.example.id
 }
 
 resource "azurerm_key_vault_secret" "openai_key" {
